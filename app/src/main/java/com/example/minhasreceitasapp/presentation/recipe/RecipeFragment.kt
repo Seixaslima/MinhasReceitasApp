feb44e2259be088b481d30a52bf64c0794640328
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-10-17 19:19:38 -0300

    observando os states com a viewModel

--- app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.minhasreceitasapp.presentation.recipe
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.example.minhasreceitasapp.R
 import com.example.minhasreceitasapp.databinding.FragmentRecipeBinding
 import com.example.minhasreceitasapp.presentation.recipe.adapter.RecipeAdapter
+import com.example.minhasreceitasapp.presentation.recipe.adapter.RecipeState
 import com.example.minhasreceitasapp.presentation.recipe.adapter.RecipesViewModel
 
 /**
@@ -33,6 +38,7 @@ class RecipeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupListener()
         setupAdapter()
+        observerState()
     }
 
     private fun setupListener () {
@@ -45,6 +51,35 @@ class RecipeFragment : Fragment() {
         binding.rvRecipes.adapter = adapter
     }
 
+    private fun observerState() {
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                RecipeState.Loading -> {
+                    binding.pbLoading.isVisible = true
+                }
+                RecipeState.Empty -> {
+                    binding.pbLoading.isVisible = false
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.label_empty_recipes),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is RecipeState.Error -> {
+                    binding.pbLoading.isVisible = false
+                    Toast.makeText(
+                        requireContext(),
+                        state.message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is RecipeState.Success -> {
+                    binding.pbLoading.isVisible = false
+                    adapter.submitList(state.recipe)
+                }
+            }
 
+        }
+    }
 
 }
