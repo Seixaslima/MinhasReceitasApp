4441340edfbd276c13e7e62105797887acf4e1bf
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-10-17 19:05:39 -0300

    Criando viewModel Factory

--- app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.minhasreceitasapp.databinding.FragmentRecipeBinding
 import com.example.minhasreceitasapp.presentation.recipe.adapter.RecipeAdapter
+import com.example.minhasreceitasapp.presentation.recipe.adapter.RecipesViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RecipeFragment : Fragment() {
 
+    private val viewModel: RecipesViewModel by viewModels {
+        RecipesViewModel.Factory()
+    }
     private lateinit var binding: FragmentRecipeBinding
     private val adapter by lazy { RecipeAdapter()}
     override fun onCreateView(
@@ -39,5 +44,7 @@ class RecipeFragment : Fragment() {
     private fun setupAdapter () {
         binding.rvRecipes.adapter = adapter
     }
+
+
 
 }
--- app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/adapter/RecipesViewModel.kt
+++ app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/adapter/RecipesViewModel.kt
@@ -3,8 +3,13 @@ package com.example.minhasreceitasapp.presentation.recipe.adapter
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.minhasreceitasapp.data.db
+import com.example.minhasreceitasapp.data.repository.RecipeRepositoryImpl
 import com.example.minhasreceitasapp.domain.model.RecipeDomain
 import com.example.minhasreceitasapp.domain.usecase.GetAllRecipesUseCase
 import com.example.minhasreceitasapp.domain.usecase.InsertRecipesUseCase
@@ -33,4 +38,19 @@ class RecipesViewModel(
     fun insert(name: String) = viewModelScope.launch {
         insertRecipesUseCase(RecipeDomain(name=name))
     }
+
+    class Factory: ViewModelProvider.Factory {
+        override fun <T: ViewModel> create(
+            modelClass: Class<T>,
+            extras: CreationExtras
+        ): T {
+            val application = checkNotNull(extras[APPLICATION_KEY])
+            val repository = RecipeRepositoryImpl(application.db.recipeDao())
+            return RecipesViewModel (
+                getAllRecipesUseCase = GetAllRecipesUseCase(repository),
+                insertRecipesUseCase = InsertRecipesUseCase(repository)
+            ) as T
+        }
+    }
+
 }
