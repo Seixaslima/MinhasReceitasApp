7ad66ea89dfd3f7ab32eed473a3978a6037ef105
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-10-17 15:45:14 -0300

    criando a primeira view

--- app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
@@ -5,41 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.minhasreceitasapp.R
-import com.example.minhasreceitasapp.databinding.FragmentFirstBinding
+import com.example.minhasreceitasapp.databinding.FragmentRecipeBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RecipeFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+    private lateinit var binding: FragmentRecipeBinding
+    private val adapter by lazy {}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentRecipeBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+    private fun setupListener () {
+        binding.fabRecipe.setOnClickListener{
+            //TODO show Dialog
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setupAdapter () {
+        //TODO criar vinculação do adapter
     }
+
 }
